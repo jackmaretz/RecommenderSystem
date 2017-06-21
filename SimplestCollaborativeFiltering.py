8803497764259aed1de44c8c8304b2dbd4ed532b
polkox928 <polkox928@gmail.com>
2017-06-21 21:21:12 +0200

    Complete simple Collaborative filtering
    The simplest collaborative filtering is complete. It uses Manhattan
    distance to find the closest user to a given one, then gives
    recommendations. The online part is to be finalised because we have to
    decide how to give the information of the user to which we have to make
    recommendations

--- SimplestCollaborativeFiltering.py
+++ SimplestCollaborativeFiltering.py
@@ -16,6 +16,7 @@ the closest users, sorted in descending order
 
 #%%
 import pandas as pd
+import numpy as np
 
 #%%
 # Read all three datasets with the correct encoding
@@ -27,8 +28,8 @@ books = pd.read_csv('books.csv', sep=';', encoding='latin1',
 users = pd.read_csv('users.csv', sep=';', encoding='latin1', error_bad_lines=False, warn_bad_lines=False)
 #%%
 
-## Drop zero ratings as they represent nothing really
-# ratings.drop(ratings[ratings['Book-Rating'] < 1].index, inplace=True)
+# Drop zero ratings as they represent nothing really
+ratings.drop(ratings[ratings['Book-Rating'] < 1].index, inplace=True)
 
 #%%
 
@@ -48,8 +49,50 @@ for ratingEntry in ratings.index:
         if not User_ID in usersRatingsDict:
             usersRatingsDict[User_ID] = dict()
         usersRatingsDict[User_ID][ISBN] = Book_Rating
+#%%
+
+def manhattanDistance(user1, user2):
+    """ Compute the manhattan distance between user1 and user2. UserX is a 
+    dictionary of the form {'book1': 3, 'book2':5 ...} """
+    distance = 0
+    check = 0
+    for book in user1:
+        if book in user2:
+            check = 1
+            distance += abs(user1[book] - user2[book])
+    if check == 0:
+        distance = np.infty
+    return distance
+
+def computeClosestUser(userOfInterest, allUsers):
+    """ Returns a sorted list of tuples containing a user and its distance from
+    the userOfInterest. userOfInterest is just the User-ID """
+    distances = []
+    for user in allUsers:
+        if user != userOfInterest:
+            distanceBetweenUsers = manhattanDistance(allUsers[userOfInterest], allUsers[user])
+            distances.append((distanceBetweenUsers, user))
+        # sort distances based on distanceBetweenUsers
+    distances.sort()
+    return distances
+        
+
+def recommend(userOfInterest, allUsers):
+    """ Returns as recommendations the items rated by the closest user to 
+    userOfInterest, selecting the ones not rated by the userOfInterest """
+    # Find the closest user 
+    closestUser = computeClosestUser(userOfInterest, allUsers)[0][1]
     
-# Calculate the number of users that gave no rating
-usersSet = set(users["User-ID"])
-ratingsSet = set(ratings["User-ID"])
-nonRatingUsers = len(usersSet.difference(ratingsSet))
+    recommendations = []
+    
+    # Find books not rated by userOfInterest
+    closestUserRatings = allUsers[closestUser]
+    userOfInterestRatings = allUsers[userOfInterest]
+    
+    for book in closestUserRatings:
+        if not book in userOfInterestRatings:
+            recommendations.append((closestUserRatings[book], book))
+    recommendations.sort(reverse = True)
+    return recommendations
+    
+    
