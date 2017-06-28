594a12caf3e80635bb20f6551937bd63de771052
polkox928 <polkox928@gmail.com>
2017-06-28 17:40:59 +0200

    Class/content

--- CollaborativeFiltering/userBasedClass.py
+++ CollaborativeFiltering/userBasedClass.py
@@ -8,6 +8,10 @@ Created on Fri Jun 23 22:26:14 2017
 #%%
 import json
 from math import sqrt
+import numpy as np
+import copy
+from random import shuffle
+import matplotlib.pyplot as plt
 #%%
 # Importing rating dictionary
 ratings = json.loads(open("ratings_dict.json").read())
@@ -17,7 +21,8 @@ ratings = json.loads(open("ratings_dict.json").read())
 class collaborativeUserBased:
     
     def __init__(self, ratingsData):        # Initialize class
-        self.data = ratingsData
+        self.data = copy.deepcopy(ratingsData)
+        self.centered = False
         
     def cosineSimilarity(self, user1, user2):
         """ returns cosine similarity between two users, x and y, according to formula cos(x,y) = sum_i(x_i*y_i)/[norm(x)*norm(y)]
@@ -28,14 +33,18 @@ class collaborativeUserBased:
         sumy2 = 0
         
         for key in user1:
+            
+            x = user1[key]
+            sumx2 += x**2
+            
             if key in self.data[user2]:
                 
-                x = user1[key]
                 y = user2[key]
-                
                 sumxy += x*y
-                sumx2 += x**2
-                sumy2 += y**2
+                
+        for key in user2:
+            y = user2[key]
+            sumy2 += y**2
         
         similarity = sumxy/(sqrt(sumx2)*sqrt(sumy2))
         
@@ -56,7 +65,61 @@ class collaborativeUserBased:
         
         return sims[:k]
     
-    
-    
-            
+    def centerRatings(self):
+        """ Center all the users' ratings (subtract the mean rating from 
+        each one. The centered info is put to True """
+        self.originalData = copy.deepcopy(self.data)
         
+        for user in self.originalData:
+            average_rating = np.mean([int(rating) for rating in list(self.originalData[user].values())])
+            
+            for book in self.originalData[user]:
+                old_rating = int(self.data[user][book])
+                self.data[user][book] = str(round(old_rating - average_rating, 3))
+        
+        self.centered = True
+        
+        
+    def getOriginalData(self):
+        """ Set the data again to the original one. The centered info is put 
+        back to False """
+        self.data = self.originalData
+        self.centered = False
+
+    def initializeCV(self, n_folds = 5):
+        """ Create the test-sets lists to be used when performing the CV. it also  """
+        self.readyForTrain = dict()
+        self.testSet = dict()
+        
+        if self.centered == True:
+            threshold = 0
+        else:
+            threshold = 5
+            
+        for user in self.data:
+            if len(self.data[user]) >= n_folds:
+                ratings = np.array(list(self.data[user].values()), dtype = float)
+                
+                if sum(ratings > threshold) >= n_folds:
+                    self.readyForTrain[user] = dict()
+                    self.testSet[user] = dict()
+
+                    for book in self.data[user]:
+                        rating = float(self.data[user][book])
+                        if rating > threshold:
+                            self.readyForTrain[user][book] = rating
+                    
+                    toTestSet = [(book, rating) for book, rating in zip(list(self.readyForTrain[user]), list(self.readyForTrain[user].values()))]
+                       
+                    shuffle(toTestSet)
+                    raw_testList = np.array_split(toTestSet, n_folds)
+                    # Necessary elaboration because of the way np.array_split works on tuples        
+                    self.testSet[user] = [[(book, rating) for book, rating in sublist] for sublist in raw_testList]
+        
+        
+    def recommend(self, user, k_nearest = 3, n_recommendations = 5):
+        """ Give a list of n_recommendations recommended (book, 
+        predicted_rating) based on k_nearest neighbors according to 
+        cosine similarity """
+        
+                  
--- content_users_dict.py
+++ content_users_dict.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 ratings = pd.read_csv('BX-Book-Ratings.csv', sep=';', encoding='latin1')
 
 ratings = pd.read_csv('BX-Book-Ratings.csv', sep=';', encoding='latin1')
@@ -9,6 +12,15 @@ books = pd.read_csv('BX-Books.csv', sep=';', encoding='latin1', skipinitialspace
 matr = ratings.merge(books, left_on='ISBN', right_on='ISBN')
 matr_cut = matr.iloc[:1000,:]
 
-pivot = pd.pivot_table(matr_cut,values='Book-Rating',index='User-ID',columns='Book-Title')
+pivot = pd.pivot_table(matr_cut,values='Book-Rating',index='User-ID',columns='Book-Author')
 
 piv_dic = pivot.to_dict(orient="index")
+
+
+piv_dic2 = dict()
+for user in piv_dic:
+    piv_dic2[user] = dict()
+    for author in piv_dic[user]:
+
+        if not math.isnan(piv_dic[user][author]):
+            piv_dic2[user][author] = piv_dic[user][author]
