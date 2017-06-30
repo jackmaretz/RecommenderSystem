4106a1625ab8181b7079e9992e4fe5d569592555
jackmaretz <giacomomaretto@gmail.com>
2017-06-30 16:38:50 +0200

    gubub

--- ContentItemFiltering/ContentItem.py
+++ ContentItemFiltering/ContentItem.py
@@ -11,17 +11,18 @@ Created on Fri Jun 30 12:55:59 2017
 import json
 import copy
 import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+
 #%%
 # Preprocessing
-path = "/Users/paolograniero/GitHub/RecommenderSystem/CollaborativeFiltering/"
+#pathPaolo = "/Users/paolograniero/GitHub/RecommenderSystem/CollaborativeFiltering/"
+
+path = "/Users/Giacomo/Google Drive/Data Science/ADM/RecommenderSystem/CollaborativeFiltering/"
 
 books = json.loads(open(path + "books_dict.json").read())
-
+ratings = json.loads(open(path + "ratings_dict.json").read())
 users = json.loads(open(path + "users_dict.json").read())
-
-def jaccard(s1,s2):
-    return len(s1.intersection(s2))/len(s1.union(s2))
-
 
 #%%
 class contentItemFiltering:
@@ -32,19 +33,46 @@ class contentItemFiltering:
         self.centered = False
         
     def centerRatings(self):
-    """ Center all the users' ratings (subtract the mean rating from 
-    each one. The centered info is put to True """
-    self.originalRatings = copy.deepcopy(self.ratings)
+        """ Center all the users' ratings (subtract the mean rating from 
+        each one. The centered info is put to True """
+        self.originalRatings = copy.deepcopy(self.ratings)
     
-    for user in self.originalRatings:
-        average_rating = np.mean([int(rating) for rating in list(self.originalRatings[user].values())])
+        for user in self.originalRatings:
+            average_rating = np.mean([int(rating) for rating in list(self.originalRatings[user].values())])
         
-        for book in self.originalRatings[user]:
-            old_rating = int(self.ratings[user][book])
-            self.ratings[user][book] = str(round(old_rating - average_rating, 3))
+            for book in self.originalRatings[user]:
+                old_rating = int(self.ratings[user][book])
+                self.ratings[user][book] = str(round(old_rating - average_rating, 3))
+        
+        self.centered = True
+
     
-    self.centered = True
+    def nostop_tokenizer(self,text):
+        stop = stopwords.words('english')  
+        tokenizer = RegexpTokenizer(r'\w+')
+        return set([word.lower() for word in tokenizer.tokenize(text) if word not in stop])
+
+    
+    def jaccard(self,str1,str2):
+        return len(str1.intersection(str2))/len(str1.union(str2))
+        
+    def similarityTextFields(self,field1,field2):
+        field1Tok = self.nostop_tokenizer(field1)
+        field2Tok = self.nostop_tokenizer(field2)
+        return self.jaccard(field1Tok,field2Tok)
+        
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
 
-    def bagOfWords(self, field):
+        
         
